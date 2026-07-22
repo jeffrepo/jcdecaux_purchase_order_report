b7907101cc1c767433b140f560abd3f1ade25d36
Jefferson Silva <jeffer.leo93@gmail.com>
2026-07-22 12:55:03 -0600

    Add JCDecaux purchase order QWeb report

--- jcdecaux_purchase_order_report/__manifest__.py
+++ jcdecaux_purchase_order_report/__manifest__.py
@@ -9,6 +9,8 @@
     "data": [
         "views/res_config_settings_views.xml",
         "views/purchase_order_views.xml",
+        "report/purchase_order_templates.xml",
+        "report/purchase_order_report.xml",
     ],
     "installable": True,
     "application": False,
