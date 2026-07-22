e9d655901dc0911f8cbc76427230ab052a93f667
Jefferson Silva <jeffer.leo93@gmail.com>
2026-07-22 13:22:29 -0600

    Fix UTF-8 encoding in purchase order PDF

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "JCDecaux - Purchase Order Report",
     "summary": "JCDecaux purchase order PDF layout",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Purchases",
     "author": "JCDecaux",
     "license": "LGPL-3",
